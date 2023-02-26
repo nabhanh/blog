2c293b1002019e810543a6c5f36cd0b475ce4c90
Nabhan Hanif <nabhanhanif2000@gmail.com>
2023-02-26 15:09:46 +0530

    feat: inital commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+import blog from 'https://deno.land/x/blog@0.5.0/blog.tsx';
+
+
+blog({
+  title: "Nabhan Hanif",
+  author: "Nabhan Hanif",
+  description: "My blog about programming, tech, and life.",
+  links: [
+    { title: "Email", url: "mailto:nabhanhanif2000@gmail.com" },
+    { title: "GitHub", url: "https://github.com/nabhanh" },
+    { title: "LinkedIn", url: "https://www.linkedin.com/in/nabhanh/" },
+  ],
+  theme: 'auto',
+});
+
+
---
+++ posts/about_me.md
@@ -0,0 +1,6 @@
+---
+title: About Me
+publish_date: 2023-02-26
+---
+
+Hello, my name is Nabhan Hanif.
